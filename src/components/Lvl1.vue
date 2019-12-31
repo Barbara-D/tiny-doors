7bba511a602af4231b3dce7f4cf3bce327ca08ca
Barbara-D <37987504+Barbara-D@users.noreply.github.com>
2019-12-31 14:10:49 +0100

    Added level 1
    -added the image for first level, moved level information to components, added space for message and level title

---
+++ src/components/Lvl1.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="card">
+			<header class="card-header">
+				<p class="card-header-title is-right">
+					Level 1
+				</p>
+			</header>
+			<div class="card-image">
+				<figure class="image is-16by9">
+					<img src="../assets/l1.png" alt="Level image">
+				</figure>
+			</div>
+			<div class="card-content">
+				Click on something in the room.
+			</div>
+			<footer class="card-footer">
+				<span class="card-footer-item">Inventory:</span>
+				<a href="#" class="card-footer-item">1</a>
+				<a href="#" class="card-footer-item">2</a>
+				<a href="#" class="card-footer-item">3</a>
+				<a href="#" class="card-footer-item">4</a>
+				<a href="#" class="card-footer-item">5</a>
+				<a href="#" class="card-footer-item">6</a>
+
+			</footer>
+		</div>
+</template>
--- src/views/Play.vue
+++ src/views/Play.vue
@@ -1,24 +1,8 @@
 <template>
 	<div id="play">
 		<div class="columns">
-  <div class="column is-8 is-offset-2">
-		<div class="card">
-			<div class="card-image">
-				<figure class="image is-16by9">
-					<img src="https://bulma.io/images/placeholders/640x360.png" alt="Placeholder image">
-				</figure>
-			</div>
-			<footer class="card-footer">
-				<span class="card-footer-item">Inventory:</span>
-				<a href="#" class="card-footer-item">1</a>
-				<a href="#" class="card-footer-item">2</a>
-				<a href="#" class="card-footer-item">3</a>
-				<a href="#" class="card-footer-item">4</a>
-				<a href="#" class="card-footer-item">5</a>
-				<a href="#" class="card-footer-item">6</a>
-
-			</footer>
-		</div>
+			<div class="column is-8 is-offset-2">
+				<lvl1></lvl1>
 
 		<br/><b-button type="is-info" size="is-medium" icon-left="arrow-left-bold" tag="router-link" to="/" outlined>GO BACK</b-button>
 		</div>
@@ -27,10 +11,11 @@
 </template>
 
 <script>
-
+import lvl1 from '@/components/Lvl1.vue'
 export default{
 name: 'play',
 components:{
+	lvl1,
 
 }
 }
